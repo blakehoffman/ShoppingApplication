16e045d1b027d6391d3a657086decb213485a659
blakehoffman <hoffman9134>
2021-08-05 07:01:08 -0500

    Discounts db schema

---
+++ Database/Tables/Discounts/Discounts.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Discounts]
+(
+	Id						uniqueidentifier	NOT NULL,
+	ClusterId				int					NOT NULL IDENTITY UNIQUE,
+	[Name]					varchar(100)		NOT NULL,
+	Amount					int					NOT NULL,
+	[SysStartTime]			datetime2			GENERATED ALWAYS AS ROW START NOT NULL,
+	[SysEndTime]			datetime2			GENERATED ALWAYS AS ROW END   NOT NULL,
+	PERIOD FOR SYSTEM_TIME (SysStartTime, SysEndTime),
+	CONSTRAINT pkDiscounts PRIMARY KEY NONCLUSTERED (Id),
+	INDEX ixDiscountsClusterId CLUSTERED (ClusterId)
+)
+WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.DiscountsHistory));
+GO
---
+++ Database/Tables/Discounts/DiscountsHistory.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DiscountsHistory]
+(
+	Id					uniqueidentifier		NOT NULL,
+	ClusterId			int						NOT NULL,
+	[Name]				varchar(100)			NOT NULL,
+	Amount				int						NOT NULL,
+	SysStartTime		datetime2				NOT NULL,
+	SysEndTime			datetime2				NOT NULL,
+)
