0f054baa07053619fd331317cd7b68d9b8414a79
blakehoffman <hoffman9134>
2022-04-10 09:14:25 -0500

    test

--- Database/Tables/Administrators/AdministratorsHistory.sql
+++ Database/Tables/Administrators/AdministratorsHistory.sql
@@ -2,6 +2,6 @@
 (
     Id              int             NOT NULL,
     Email           varchar(100)    NOT NULL,
-    SysStartTime	datetime2		NOT NULL,
-    SysEndTime		datetime2		NOT NULL,
+    SysStartTime    datetime2		NOT NULL,
+    SysEndTime      datetime2		NOT NULL,
 )
